b9e9e02300a861d0c6f1d9d329273c0d74947bca
maurozem <maurozem@brq.com>
2020-07-24 16:30:58 -0300

    compartilhamento de imageView como JPG OK II

--- app/src/main/java/ms/zem/firebasecompleto/extensions/ImageView.kt
+++ app/src/main/java/ms/zem/firebasecompleto/extensions/ImageView.kt
@@ -1,12 +1,9 @@
 package ms.zem.firebasecompleto.extensions
 
-import android.content.ContentResolver
-import android.content.ContentValues
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
 import android.net.Uri
-import android.os.Build
 import android.provider.MediaStore
 import android.widget.ImageView
 import ms.zem.firebasecompleto.utils.AlertDialogUtil
@@ -14,11 +11,15 @@ import java.io.ByteArrayOutputStream
 
 
 fun ImageView.compartilhar() {
-    if (this.drawable != null){
+    if (this.drawable != null) {
         val bitmap = (this.drawable as BitmapDrawable).bitmap
         bitmap.compress(Bitmap.CompressFormat.JPEG, 100, ByteArrayOutputStream())
-        val uri = Uri.parse(MediaStore.Images.Media.insertImage(this.context.contentResolver,
-            bitmap, "Imagem", null))
+        val uri = Uri.parse(
+            MediaStore.Images.Media.insertImage(
+                this.context.contentResolver,
+                bitmap, "Imagem", null
+            )
+        )
         val intent = Intent(Intent.ACTION_SEND)
         intent.type = "image/jpeg"
         intent.putExtra(Intent.EXTRA_STREAM, uri)
@@ -27,26 +28,4 @@ fun ImageView.compartilhar() {
         AlertDialogUtil.init(this.context)
             .erro("sem imagem para compartilhar")
     }
-}
-
-fun ImageView.compartilhar2020() { /// não esta ok
-    val intent = Intent(Intent.ACTION_SEND)
-    intent.type = "image/jpeg"
-    val bitmap = (this.drawable as BitmapDrawable).bitmap
-    val bytes = ByteArrayOutputStream()
-    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bytes)
-
-    val uri = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-        val contentValues = ContentValues()
-        contentValues.put(MediaStore.MediaColumns.TITLE, "Compartilhar")
-        contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
-        contentValues.put("image", bytes.toByteArray())
-        val resolver: ContentResolver = this.context.contentResolver
-        resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
-    } else {
-        Uri.parse(MediaStore.Images.Media.insertImage(
-            this.context.contentResolver, bitmap, "Compartilhar", null))
-    }
-    intent.putExtra(Intent.EXTRA_STREAM, uri)
-    this.context.startActivity(Intent.createChooser(intent, "Compartilhar imagem"))
 }
--- app/src/main/java/ms/zem/firebasecompleto/ui/storage/StorageDownloadActivity.kt
+++ app/src/main/java/ms/zem/firebasecompleto/ui/storage/StorageDownloadActivity.kt
@@ -1,29 +1,18 @@
 package ms.zem.firebasecompleto.ui.storage
 
-import android.content.ContentValues
-import android.content.Intent
-import android.graphics.Bitmap
-import android.graphics.drawable.BitmapDrawable
-import android.net.Uri
-import android.os.Build
 import android.os.Bundle
-import android.provider.MediaStore
 import android.view.Menu
 import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_storage_download.*
 import kotlinx.android.synthetic.main.toolbar.toolbar
 import ms.zem.firebasecompleto.R
 import ms.zem.firebasecompleto.extensions.compartilhar
-import ms.zem.firebasecompleto.extensions.compartilhar2020
 import ms.zem.firebasecompleto.ui.BaseActivity
 import ms.zem.firebasecompleto.utils.AlertDialogUtil
 import ms.zem.firebasecompleto.utils.StorageUtil
-import java.io.ByteArrayOutputStream
+
 
 class StorageDownloadActivity : BaseActivity() {
-
-    private val urlOld =
-        "https://firebasestorage.googleapis.com/v0/b/pessoal-estudo.appspot.com/o/imagem%2Fpilares.png?alt=media&token=b7678856-e984-4732-ac4c-761eff7e8318"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,8 +26,8 @@ class StorageDownloadActivity : BaseActivity() {
             StorageUtil.downloadImage("pilares.png", imgDownload, pgbDownload)
         }
         btnRemover.setOnClickListener {
-            StorageUtil.deleteImage("pilares.png", imgDownload, pgbDownload){ sucesso ->
-                if (sucesso){
+            StorageUtil.deleteImage("pilares.png", imgDownload, pgbDownload) { sucesso ->
+                if (sucesso) {
                     AlertDialogUtil.init(this)
                         .sucesso("imagem removida")
                 } else {
@@ -57,7 +46,7 @@ class StorageDownloadActivity : BaseActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.gerarPDF -> {
-                gerarPDF()
+                pdf()
                 return true
             }
             R.id.compartilhar -> {
@@ -68,11 +57,12 @@ class StorageDownloadActivity : BaseActivity() {
         return super.onOptionsItemSelected(item)
     }
 
+    fun pdf() {
+        mensagem(this, "não implementado")
+    }
+
     private fun compartilhar() {
         imgDownload.compartilhar()
     }
 
-    private fun gerarPDF() {
-        mensagem(this, "gerar pdf")
-    }
 }
